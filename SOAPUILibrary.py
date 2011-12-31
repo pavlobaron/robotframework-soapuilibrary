7543ed3b1fd7a7a677233f021e35f36acbcb8c62
pavlobaron <pb@pbit.org>
2011-12-31 15:44:12 +0100

    initial commit, v. 0.1

---
+++ SOAPUILibrary.py
@@ -0,0 +1,15 @@
+from com.eviware.soapui.tools import (SoapUITestCaseRunner)
+
+class SOAPUILibrary:
+
+    ROBOT_LIBRARY_SCOPE = 'TEST SUITE'
+    ROBOT_LIBRARY_VERSION = '0.1'
+
+    def run_project(self, prj):
+        runner = SoapUITestCaseRunner()
+        runner.setProjectFile(prj)
+        if not runner.run():
+            raise AssertionError('FAIL: failed to run')
+
+        if runner.getFailedTests().size() != 0:
+            raise AssertionError('FAIL: ' + str(n) + ' tests failed')
