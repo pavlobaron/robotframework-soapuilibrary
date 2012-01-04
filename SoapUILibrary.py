e18829ddbdceb5302ffd015109e8ab077c368a58
pavlobaron <pb@pbit.org>
2012-01-04 21:17:52 +0100

    v.2.0 - test suites and cases are now separately runnable

--- SoapUILibrary.py
+++ SoapUILibrary.py
@@ -2,14 +2,23 @@ from com.eviware.soapui.tools import (SoapUITestCaseRunner)
 
 class SoapUILibrary:
 
-    ROBOT_LIBRARY_SCOPE = 'TEST SUITE'
-    ROBOT_LIBRARY_VERSION = '0.1.1'
+    ROBOT_LIBRARY_SCOPE = 'TEST CASE'
+    ROBOT_LIBRARY_VERSION = '0.2'
 
-    def run_project(self, prj):
-        runner = SoapUITestCaseRunner()
-        runner.setProjectFile(prj)
-        if not runner.run():
+    def soapui_project(self, prj):
+        self.__runner = SoapUITestCaseRunner()
+        self.__runner.setProjectFile(prj)
+
+    def soapui_suite(self, s):
+        self.__runner.setTestSuite(s)
+
+    def soapui_case(self, c):
+        self.__runner.setTestCase(c)
+
+    def soapui_run(self):
+        if not __runner.run():
             raise AssertionError('FAIL: failed to run')
 
-        if runner.getFailedTests().size() != 0:
+        n = self.__runner.getFailedTests().size()
+        if n != 0:
             raise AssertionError('FAIL: ' + str(n) + ' tests failed')
