29ba62e4b78beec5b23af21936d4b73a4948998c
pavlobaron <pb@pbit.org>
2012-01-04 11:25:22 +0100

    v 0.1.1

--- SoapUILibrary.py
+++ SoapUILibrary.py
@@ -3,7 +3,7 @@ from com.eviware.soapui.tools import (SoapUITestCaseRunner)
 class SoapUILibrary:
 
     ROBOT_LIBRARY_SCOPE = 'TEST SUITE'
-    ROBOT_LIBRARY_VERSION = '0.1'
+    ROBOT_LIBRARY_VERSION = '0.1.1'
 
     def run_project(self, prj):
         runner = SoapUITestCaseRunner()
